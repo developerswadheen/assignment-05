3191377630286b4fd5de822392d677df4fecb2f3
developerswadheen <swadheenroy99@gmail.com>
2024-02-19 23:31:14 +0600

    added toggle

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -7,7 +7,7 @@ let seatLeft = document.getElementById("SeatLeft");
 let seatCount = document.getElementById("seatCount");
 let totalPrice = parseInt(document.getElementById("totalPrice").innerText);
 let grandTotal = parseInt(document.getElementById("grandTotal").innerText);
-let applyBtn = document.getElementById('applyBtn');
+let applyBtn = document.getElementById("applyBtn");
 
 let busSeat = document.getElementsByClassName("seatBtn");
 
@@ -16,7 +16,7 @@ for (let i of busSeat) {
     console.log(i.innerText);
     seatCount.innerText = count;
     count++;
-
+  
     seatLeft.innerText = decreaseSeat;
     decreaseSeat--;
 
@@ -34,18 +34,19 @@ for (let i of busSeat) {
     array.push(seatNumber);
 
     let arrayLength = array.length;
-    console.log(arrayLength)
+    console.log(arrayLength);
 
-    if(arrayLength >= 5){
-      alert('You can Select Max 4 Seats')
-      i.classList.remove('bg-[#1DD100]')
+    if (arrayLength >= 5) {
+      alert("You can Select Max 4 Seats");
+      i.classList.remove("bg-[#1DD100]");
+      return;
     }
-    if(arrayLength > 3){
-      applyBtn.removeAttribute('disabled', true)
+    if (arrayLength > 3) {
+      applyBtn.removeAttribute("disabled", true);
     }
-
+    
     p1.innerText = seatNumber;
-    p2.innerText = 'Economy';
+    p2.innerText = "Economy";
     p3.innerText = 550;
 
     div.appendChild(p1);
@@ -62,16 +63,29 @@ for (let i of busSeat) {
 
     document.getElementById("totalPrice").innerText = totalPrice;
     document.getElementById("grandTotal").innerText = totalPrice;
-
   });
 }
 
+let coupon = document.getElementById("coupon");
 
-let coupon = document.getElementById('coupon');
-
-function applyCoupon(){
- if(coupon.value == 'NEW15'){
-  let hide = document.getElementById('hide');
-  hide.classList.add('hidden')
- }
+function applyCoupon() {
+  if (coupon.value == "NEW15") {
+    let hide = document.getElementById("hide");
+    hide.classList.add("hidden");
+    // let totalTicketPrice = parseInt(document.getElementById("totalPrice").innerText);
+    discountPrice = totalPrice - totalPrice * 0.15;
+    document.getElementById("grandTotal").innerText = discountPrice;
+  }
+  else if(coupon.value == "Couple 20"){
+    let hide = document.getElementById("hide");
+    hide.classList.add("hidden");
+    // let totalTicketPrice = parseInt(document.getElementById("totalPrice").innerText);
+    discountPrice = totalPrice - totalPrice * 0.20;
+    document.getElementById("grandTotal").innerText = discountPrice;
+  }
 }
+
+document.getElementById('buyTicketBtn').addEventListener('click', function() {
+  // Scroll to the ticket sell section smoothly
+  document.getElementById('buyTicket').scrollIntoView({ behavior: 'smooth' });
+});
